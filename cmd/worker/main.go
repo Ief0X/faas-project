17aae044cc951599d2e90f8790852dcffb414f63
Jefferson Caiza <jpcaijam@upv.edu.es>
2025-01-17 20:01:20 +0100

    Time increased for pull docker image

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -60,7 +60,7 @@ func main() {
 				return
 			}
 
-			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 			defer cancel()
 
 			hostConfig := &container.HostConfig{
